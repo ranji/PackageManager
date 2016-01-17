c74366b931b4d15407d5bb837a364da18f439185
ranji <ranjit.thayyil@gmail.com>
2016-01-17 10:17:49 -0800

    Initial Commit

---
+++ PackageManager/PackageManager.h
@@ -0,0 +1,19 @@
+//
+//  PackageManager.h
+//  PackageManager
+//
+//  Created by RANJIT THAYYIL on 1/17/16.
+//  Copyright © 2016 freehub. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PackageManager.
+FOUNDATION_EXPORT double PackageManagerVersionNumber;
+
+//! Project version string for PackageManager.
+FOUNDATION_EXPORT const unsigned char PackageManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PackageManager/PublicHeader.h>
+
+
