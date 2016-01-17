686739f7318ff6b4f29b9f75d388aee4fbaa4741
ranji <ranjit.thayyil@gmail.com>
2016-01-17 10:24:00 -0800

    initial commit with an empty test and dependency resolver class

---
+++ PackageManager/DependencyResolver.swift
@@ -0,0 +1,13 @@
+//
+//  DependencyResolver.swift
+//  PackageManager
+//
+//  Created by RANJIT THAYYIL on 1/17/16.
+//  Copyright © 2016 freehub. All rights reserved.
+//
+
+import Foundation
+
+class DependencyResolver {
+    
+}
--- PackageManagerTests/DependencyResolverTests.swift
+++ PackageManagerTests/DependencyResolverTests.swift
@@ -1,16 +1,15 @@
 //
-//  PackageManagerTests.swift
-//  PackageManagerTests
+//  DependencyResolverTests.swift
+//  PackageManager
 //
 //  Created by RANJIT THAYYIL on 1/17/16.
 //  Copyright © 2016 freehub. All rights reserved.
 //
 
 import XCTest
-@testable import PackageManager
 
-class PackageManagerTests: XCTestCase {
-    
+class DependencyResolverTests: XCTestCase {
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,17 +19,19 @@ class PackageManagerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        _ =  DependencyResolver()
+        XCTAssert(true)
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
